80efffc6f3fffcd0e5f54fd31baad88d4fa58539
ahndohun <ahndohun1@gmail.com>
2019-09-03 20:45:26 +0900

    :lipstick: Modify mainpage ui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
   <div>
-    <h2>
-        Home
-    </h2>
+    <h1>Lunch is mundle</h1>
+    <Link to="/room/create">시작하기</Link>
   </div>
 );
 
