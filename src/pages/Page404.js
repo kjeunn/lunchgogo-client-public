329575eb9696cc3a49aac49e3dc0fb45e93dd768
ahndohun <ahndohun1@gmail.com>
2019-09-11 18:27:06 +0900

    :lipstick: Modify 디자인 업그레이드03

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo404 from 'assets/images/lunchgogo-404Page.png';
 
 const Page404 = () => (
-  <div>
-    <h1>404 Page</h1>
-    <Link to="/">Home으로 돌아가기</Link>
+  <div className="page404">
+    <div className="page404__inner">
+      <div className="page404__title"><img src={logo404} /></div>
+      <p className="page404__text">
+존재하지 않는 페이지 이거나
+<br></br>
+{' '}
+이미 없어진 방이에요.
+</p>
+      <Link className="page404__button" to="/">홈으로 돌아가기</Link>
+    </div>
   </div>
 );
 
